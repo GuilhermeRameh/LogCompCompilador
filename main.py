949a3ad3d46847bd26d01f9ec85b2e3219039827
GuilhermeRameh <guilhermedr@al.insper.edu.br>
2023-02-27 17:11:26 -0300

    Faltou commitar só

--- main.py
+++ main.py
@@ -1,24 +1,138 @@
 import sys
 
-def somaSubtrai(operacoes):
-    totalsum = 0
-    stemp = ""
-    last_operator = "+"
-    for i in range(len(operacoes)):
-        w=operacoes[i]
-        if w not in ["+", "-"]:
-            stemp = stemp + w
-        if w in ["+", "-"] or i==len(operacoes)-1:
-            if (w in ["+", "-"] and i==len(operacoes)-1):
-                sys.stderr.write("Erro de Semântica:\n  >Operando é o último item da string.")
-                sys.exit(1)
-            if last_operator == "+":
-                totalsum += int(stemp)
-            elif last_operator == "-":
-                totalsum -= int(stemp)
-            last_operator = w
-            stemp = ""
-    print(totalsum)
+################# TOKEN CLASS #####################
+class Token:
+    def __init__(self, tipo, valor) -> None:
+        self.tipo = tipo
+        self.valor = valor
 
-args = sys.argv[1:][0]
-somaSubtrai(args)
+############ TOKENIZER CLASS ######################
+class Tokenizer:
+    def __init__(self, source) -> None:
+        self.source = source
+        self.position = -1
+        self.next = None
+        self.selectNext()
+
+    def selectNext(self):
+        if self.position == -1:
+            self.position += 1  
+        if self.position >= len(self.source):
+            tipo = "EOF"
+            valor = ""
+        else:
+            letra = self.source[self.position]
+            valor = ""
+            while letra == " ":
+                self.position += 1
+                letra = self.source[self.position]
+            
+            if letra.isnumeric():
+                while letra in ["0","1","2","3","4","5","6","7","8","9"]:
+                    tipo = "INT"
+                    valor += letra
+                    self.position += 1
+                    if self.position >= len(self.source):
+                        break
+                    letra = self.source[self.position]
+
+            elif letra == "+":
+                tipo = "PLUS"
+                valor = letra
+                self.position += 1
+            elif letra == "-":
+                tipo = "MINUS"
+                valor = letra
+                self.position += 1
+            elif letra == "*":
+                tipo = "MULT"
+                valor = letra
+                self.position += 1
+            elif letra == "/":
+                tipo = "DIV"
+                valor = letra
+                self.position += 1
+
+        tokenCreate = Token(tipo, valor)
+        self.next = tokenCreate
+
+############## Parser Class ########################
+
+class Parser:
+    def __init__(self):
+        self.tokenizer = Tokenizer
+
+    def parseTerm(self):
+        buffer = 0
+        #Se o token atual for numero
+        if self.tokenizer.next.tipo == "INT":
+            #copia para resultado e pega proximo valor
+            buffer = int(self.tokenizer.next.valor)
+            self.tokenizer.selectNext()
+            #enquanto token for + ou -
+            while self.tokenizer.next.tipo in ["MULT", "DIV"]:
+                #Se for +
+                if self.tokenizer.next.tipo == "MULT":
+                    self.tokenizer.selectNext()
+                    if self.tokenizer.next.tipo == "INT":
+                        buffer *= int(self.tokenizer.next.valor)
+                    else:
+                        raise Exception(f"ERRO SINTÁTICO:\n    >Era esperado um TOKEN do tipo 'INT', mas recebeu '{self.tokenizer.next.tipo}'")
+                #Se for -
+                if self.tokenizer.next.tipo == "DIV":
+                    self.tokenizer.selectNext()
+                    if self.tokenizer.next.tipo == "INT":
+                        buffer = buffer//int(self.tokenizer.next.valor)
+                    else:
+                        raise Exception(f"ERRO SINTÁTICO:\n    >Era esperado um TOKEN do tipo 'INT', mas recebeu '{self.tokenizer.next.tipo}'")
+                self.tokenizer.selectNext()
+            return buffer
+        else:
+            raise Exception("ERRO SINTÁTICO:\n    >O primeiro TOKEN da cadeia deve ser do tipo 'INT'.")
+
+    def parseExpression(self):
+
+        resultado = self.parseTerm()
+
+        #enquanto token for + ou -
+        while self.tokenizer.next.tipo in ["PLUS", "MINUS", "MULT", "DIV"]:
+            #Se for +
+            if self.tokenizer.next.tipo == "PLUS":
+                self.tokenizer.selectNext()
+                resultado += self.parseTerm()
+            #Se for -
+            if self.tokenizer.next.tipo == "MINUS":
+                self.tokenizer.selectNext()
+                resultado -= self.parseTerm()
+        return resultado
+
+    def run(self, code):
+        self.tokenizer = Tokenizer(code)
+        toReturn = self.parseExpression()
+        #checa se terminou de consumir
+        if self.tokenizer.next.tipo == "EOF":
+            return toReturn
+        else:
+            raise Exception(f"ERRO PARSER:\n O parser saiu e não consumiu todos os tokens.")
+
+
+
+#--------------------------------------------------------#
+#                         MAIN                           #
+#--------------------------------------------------------#
+
+DEBUG = False
+debugCadeia = "2+2-5+4"
+
+def main():
+    if DEBUG==True:
+        cadeia = debugCadeia
+    else:
+        cadeia = sys.argv[1]
+
+    parser = Parser()
+    print(parser.run(cadeia))
+
+
+if __name__ == "__main__":
+    main()
